c643dad3ee7e29f7ac60e9b2fdf7d7f9b683a225
Omar <omarqhayyam12@gmail.com>
2025-01-13 22:31:06 +0530

    Spotify app completed

---
+++ Terraform/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    spotify = {
+      source = "conradludgate/spotify"
+      version = "0.2.7"
+    }
+  }
+}
+
+provider "spotify" {
+  api_key="2K4HW1yeFP0CX-Ufmv8kp_mhHtLIZLwNN00BexiNIn-KhtjQnHY7ORZxb5qZb7e7"
+  # Configuration options
+}
